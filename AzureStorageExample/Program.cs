c55ebf27efcc3bb1c14f3249e45bba8e980f6d66
tombiddulph <tombiddulph@gmail.com>
2017-11-01 16:47:55 +0000

    Add project files.

---
+++ AzureStorageExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureStorageExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
